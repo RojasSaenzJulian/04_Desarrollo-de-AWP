75b12dfc07ca5cb8b3ec3d4d78a97be8e8aa4a6d
RojasSaenzJulian <105955487+RojasSaenzJulian@users.noreply.github.com>
2023-06-03 00:30:45 -0600

    Practica02 Finalizada
    -Hay un mínimo de 8 páginas (contando el index)
    -Páginas con música de fondo y elementos con sonido
    -Se aplicó videos externos, imagenes locales (en su mayoría) y externas´, audios locales, enlaces internos y externos

---
+++ Material/Semana 03/InitializrSpringbootProject/src/main/java/com/Practica02/Practica0/Practica02Application.java
@@ -0,0 +1,13 @@
+package com.Practica02.Practica0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica02Application.class, args);
+	}
+
+}
